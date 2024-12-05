75e6ff1823ad12999b1030ba14f37dc324e2daf6
unknown <celiagarridoherrera@gmail.com>
2024-12-05 14:06:49 +0100

    add_menu

---
+++ menu.js
@@ -0,0 +1,4 @@
+document.getElementById('dropdown_button').addEventListener('click', function() {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('open');
+})
