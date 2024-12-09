b0fea4bd5b1f2e4933c1263c0ef0c4b0c1fd51fd
unknown <martabernardozamora@gmail.com>
2024-12-09 15:41:07 +0100

    feature/disable_button

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 document.getElementById('menu').addEventListener('change', function () {
   const menuList = document.getElementById('menu_list');
-  enuList.style.display = this.checked ? 'block' : 'none';
+  menuList.style.display = this.checked ? 'block' : 'none';
 })
 
+let condition_color = false;
+let condition_unit = false;
 document.querySelector(".add_to_cart").addEventListener('click', function (){
-  let unit = Number(document.querySelector(".quantity_selector").value);
-  let cart_num = Number(document.querySelector("#cart_pop").textContent);
-  document.querySelector("#cart_pop").textContent = unit+cart_num;
+  if(condition_color && condition_unit){
+    let unit = Number(document.querySelector(".quantity_selector").value);
+    let cart_num = Number(document.querySelector("#cart_pop").textContent);
+    document.querySelector("#cart_pop").textContent = unit+cart_num;
+    document.querySelector(".quantity_selector").value=0;
+    condition_color = false;
+    condition_unit = false;
+    alert("Producto añadido correctamente");
+  }else if (!condition_color) {
+    alert("Elija un color");
+  } else {
+    alert("Elija una cantidad");
+  }
 });
-document.querySelector(".color").forEach(element => {
-  selector.addEventListener('click', function (){
-    alert("aqui");
+function disable(){
+  let colors=document.querySelectorAll(".color");
+  colors.forEach(color => {
+    color.addEventListener('click', function (){
+      condition_color = true;
+    });
   });
-});
+  document.querySelector(".quantity_selector").addEventListener('change', function (){
+    if(document.querySelector(".quantity_selector").value>0){
+      condition_unit = true;
+    }else condition_unit = false;
+  });
+}
+disable();
